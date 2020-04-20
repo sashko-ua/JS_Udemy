dbd5fd03e5aaa1ac702cc4526373982b6b0bb62c
Sashko Shablenskyi <56129170+sashko-ua-94@users.noreply.github.com>
2020-04-20 10:44:51 +0300

    lesson 4

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,31 @@
 'use strict';
+let money, time;
 
-let money = +prompt('Ваш бюджет на місяць?', ''),
+function start () {
+	money = +prompt('Ваш бюджет на місяць?', '');
 	time = prompt("Введіть дату в форматі YYYY-MM-DD", '');
+
+	while(isNaN(money) || money == "" || money == null) {
+		money = +prompt('Ваш бюджет на місяць?', '');
+	}
+}
+start();
+
+function chooseExpenses() {
+	for (let i = 0; i < 2; i++) {
+		let a = prompt("Обов'язкова стаття витрат", ''),
+			b = prompt("Скільки коштуватиме?", '');
+	
+		if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
+			console.log('Done');
+			appData.expenses[a] = b;
+		} else {
+			console.log('Inpit error');
+			i--;
+		}
+	}
+}
+chooseExpenses();
 
 let	appData = {
 	budget: money,
@@ -9,64 +33,10 @@ let	appData = {
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
-	savings: false
+	savings: true
 };
- 
-for (let i = 0; i < 2; i++) {
-	let a = prompt("Обов'язкова стаття витрат", ''),
-		b = prompt("Скільки коштуватиме?", '');
 
-	if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-		console.log('Done');
-		appData.expenses[a] = b;
-	} else {
-		console.log('Inpit error');
-		i--;
-	}
-}
-
-
-
-// __________WHILE__________
-let i = 0;
-while (i < 2) {
-	let a = prompt("Обов'язкова стаття витрат", ''),
-		b = prompt("Скільки коштуватиме?", '');
-	
-	if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-		console.log('Done');
-		appData.expenses[a] = b;
-	} else {
-		console.log('Inpit error');
-		i--;
-	}
-
-	i++;
-}
-
-
-
-// __________DO__________
-// let i = 0;
-do {
-	let a = prompt("Обов'язкова стаття витрат", ''),
-		b = prompt("Скільки коштуватиме?", '');
-
-	if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-		console.log('Done');
-		appData.expenses[a] = b;
-	} else {
-		console.log('Inpit error');
-		i--;
-	}
-
-	i++;
-}
-while(i < 2);
-
-
-
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
 alert("Щоденний бюджет: " + appData.moneyPerDay);
 
@@ -79,3 +49,51 @@ if (appData.moneyPerDay < 100) {
 } else {
 	console.log("Error");
 }
+
+function checkSavings() {
+	if (appData.savings == true) {
+		let save = +prompt("Яка сума накопичень?"),
+			persent = +prompt("Під який відсоток?");
+
+		appData.monthIncome = save/100/12+persent;
+		alert("Дохід з дкепозиту: " + appData.monthIncome);
+	}
+}
+checkSavings();
+
+// __________WHILE__________
+// let i = 0;
+// while (i < 2) {
+// 	let a = prompt("Обов'язкова стаття витрат", ''),
+// 		b = prompt("Скільки коштуватиме?", '');
+	
+// 	if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
+// 		console.log('Done');
+// 		appData.expenses[a] = b;
+// 	} else {
+// 		console.log('Inpit error');
+// 		i--;
+// 	}
+
+// 	i++;
+// }
+
+
+
+// __________DO__________
+// let i = 0;
+// do {
+// 	let a = prompt("Обов'язкова стаття витрат", ''),
+// 		b = prompt("Скільки коштуватиме?", '');
+
+// 	if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
+// 		console.log('Done');
+// 		appData.expenses[a] = b;
+// 	} else {
+// 		console.log('Inpit error');
+// 		i--;
+// 	}
+
+// 	i++;
+// }
+// while(i < 2);
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,5 @@
 'use strict';
-
-let money = +prompt('Ваш бюджет на місяць?', ''),
-	time = prompt("Введіть дату в форматі YYYY-MM-DD", '');
+let money, time;
 
 let	appData = {
 	budget: money,
@@ -9,23 +7,65 @@ let	appData = {
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
-	savings: false
+	savings: true
 };
- 
-for (let i = 0; i < 2; i++) {
-	let a = prompt("Обов'язкова стаття витрат", ''),
-		b = prompt("Скільки коштуватиме?", '');
 
-	if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-		console.log('Done');
-		appData.expenses[a] = b;
-	} else {
-		console.log('Inpit error');
-		i--;
+function start () {
+	money = +prompt('Ваш бюджет на місяць?', '');
+	time = prompt("Введіть дату в форматі YYYY-MM-DD", '');
+
+	while(isNaN(money) || money == "" || money == null) {
+		money = +prompt('Ваш бюджет на місяць?', '');
 	}
 }
+start();
 
+function chooseExpenses() {
+	for (let i = 0; i < 2; i++) {
+		let a = prompt("Обов'язкова стаття витрат", ''),
+			b = prompt("Скільки коштуватиме?", '');
+	
+		if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
+			console.log('Done');
+			appData.expenses[a] = b;
+		} else {
+			console.log('Inpit error');
+			i--;
+		}
+	}
+}
+chooseExpenses();
 
+function detectDayBudget() {
+	appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+	alert("Щоденний бюджет: " + appData.moneyPerDay);
+}
+detectDayBudget();
+
+function detectLevel() {
+	if (appData.moneyPerDay < 100) {
+		console.log("Мінімальний рівень достаку");
+	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay <2000) {
+		console.log("Середній рівень достатку");
+	} else if (appData.moneyPerDay > 2000) {
+		console.log("Високий рівень достатку");
+	} else {
+		console.log("Error");
+	}
+}
+detectLevel();
+
+function checkSavings() {
+	if (appData.savings == true) {
+		let save = +prompt("Яка сума накопичень?"),
+			persent = +prompt("Під який відсоток?");
+
+		appData.monthIncome = save/100/12+persent;
+		alert("Дохід з дкепозиту: " + appData.monthIncome);
+	}
+}
+checkSavings();
 
 // __________WHILE__________
 // let i = 0;
@@ -63,19 +103,3 @@ for (let i = 0; i < 2; i++) {
 // 	i++;
 // }
 // while(i < 2);
-
-
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Щоденний бюджет: " + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-	console.log("Мінімальний рівень достаку");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay <2000) {
-	console.log("Середній рівень достатку");
-} else if (appData.moneyPerDay > 2000) {
-	console.log("Високий рівень достатку");
-} else {
-	console.log("Error");
-}
