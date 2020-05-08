2f4cf4856895bfbd449233f123ec35f106bc01f2
Sashko Shablenskyi <56129170+sashko-ua-94@users.noreply.github.com>
2020-05-08 14:05:20 +0300

    again from firsta lesson

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,99 +1,21 @@
-'use strict';
-let money, time;
+"use strict";
 
-function start () {
-	money = +prompt('Ваш бюджет на місяць?', '');
-	time = prompt("Введіть дату в форматі YYYY-MM-DD", '');
+let money = prompt( 'Ваш бюджет?', '' ),
+    time = prompt( 'Введіть дату в форматі DD-MM-YYYY', '' );
 
-	while(isNaN(money) || money == "" || money == null) {
-		money = +prompt('Ваш бюджет на місяць?', '');
-	}
-}
-start();
-
-function chooseExpenses() {
-	for (let i = 0; i < 2; i++) {
-		let a = prompt("Обов'язкова стаття витрат", ''),
-			b = prompt("Скільки коштуватиме?", '');
-	
-		if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-			console.log('Done');
-			appData.expenses[a] = b;
-		} else {
-			console.log('Inpit error');
-			i--;
-		}
-	}
-}
-chooseExpenses();
-
-let	appData = {
-	budget: money,
-	timeData: time,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	savings: true
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 };
 
-appData.moneyPerDay = (appData.budget / 30).toFixed();
+let a = prompt( 'Обов\'язкова стаття витрат', '' ),
+    b = prompt( 'Скальки коштує', '' ),
+    c = prompt( 'Обов\'язкова стаття витрат', '' ),
+    d = prompt( 'Скальки коштує', '' );
 
-alert("Щоденний бюджет: " + appData.moneyPerDay);
+appData.expenses[a] = b;
 
-if (appData.moneyPerDay < 100) {
-	console.log("Мінімальний рівень достаку");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay <2000) {
-	console.log("Середній рівень достатку");
-} else if (appData.moneyPerDay > 2000) {
-	console.log("Високий рівень достатку");
-} else {
-	console.log("Error");
-}
-
-function checkSavings() {
-	if (appData.savings == true) {
-		let save = +prompt("Яка сума накопичень?"),
-			persent = +prompt("Під який відсоток?");
-
-		appData.monthIncome = save/100/12+persent;
-		alert("Дохід з дкепозиту: " + appData.monthIncome);
-	}
-}
-checkSavings();
-
-// __________WHILE__________
-// let i = 0;
-// while (i < 2) {
-// 	let a = prompt("Обов'язкова стаття витрат", ''),
-// 		b = prompt("Скільки коштуватиме?", '');
-	
-// 	if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-// 		console.log('Done');
-// 		appData.expenses[a] = b;
-// 	} else {
-// 		console.log('Inpit error');
-// 		i--;
-// 	}
-
-// 	i++;
-// }
-
-
-
-// __________DO__________
-// let i = 0;
-// do {
-// 	let a = prompt("Обов'язкова стаття витрат", ''),
-// 		b = prompt("Скільки коштуватиме?", '');
-
-// 	if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-// 		console.log('Done');
-// 		appData.expenses[a] = b;
-// 	} else {
-// 		console.log('Inpit error');
-// 		i--;
-// 	}
-
-// 	i++;
-// }
-// while(i < 2);
+console.log(appData);
--- src/js/script.js
+++ src/js/script.js
@@ -1,115 +1,23 @@
-'use strict';
-let money, time;
+"use strict";
 
-let	appData = {
-	budget: money,
-	timeData: time,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	savings: true
+let money = prompt( 'Ваш бюджет?', '' ),
+    time = prompt( 'Введіть дату в форматі DD-MM-YYYY', '' );
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+    timeData: time
 };
 
-function start () {
-	money = +prompt('Ваш бюджет на місяць?', '');
-	time = prompt("Введіть дату в форматі YYYY-MM-DD", '');
+let a = prompt( 'Обов\'язкова стаття витрат', '' ),
+    b = prompt( 'Скальки коштує', '' ),
+    c = prompt( 'Обов\'язкова стаття витрат', '' ),
+    d = prompt( 'Скальки коштує', '' );
 
-	while(isNaN(money) || money == "" || money == null) {
-		money = +prompt('Ваш бюджет на місяць?', '');
-	}
-}
-start();
+appData.expenses[a] = b;
+appData.expenses[c] = d;
 
-function chooseExpenses() {
-	for (let i = 0; i < 2; i++) {
-		let a = prompt("Обов'язкова стаття витрат", ''),
-			b = prompt("Скільки коштуватиме?", '');
-	
-		if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-			console.log('Done');
-			appData.expenses[a] = b;
-		} else {
-			console.log('Inpit error');
-			i--;
-		}
-	}
-}
-chooseExpenses();
-
-function detectDayBudget() {
-	appData.moneyPerDay = (appData.budget / 30).toFixed();
-
-	alert("Щоденний бюджет: " + appData.moneyPerDay);
-}
-detectDayBudget();
-
-function detectLevel() {
-	if (appData.moneyPerDay < 100) {
-		console.log("Мінімальний рівень достаку");
-	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay <2000) {
-		console.log("Середній рівень достатку");
-	} else if (appData.moneyPerDay > 2000) {
-		console.log("Високий рівень достатку");
-	} else {
-		console.log("Error");
-	}
-}
-detectLevel();
-
-function checkSavings() {
-	if (appData.savings == true) {
-		let save = +prompt("Яка сума накопичень?"),
-			persent = +prompt("Під який відсоток?");
-
-		appData.monthIncome = save/100/12+persent;
-		alert("Дохід з дкепозиту: " + appData.monthIncome);
-	}
-}
-checkSavings();
-
-function chooseOptExpenses() {
-    for (let i = 1; i <= 3; i++) {
-        let questionOptExpenses = prompt("Статья необязательных расходов?");
-        appData.optionalExpenses[i] = questionOptExpenses;
-        console.log(appData.optionalExpenses);
-	}
-}
-chooseOptExpenses();
-
-
-// __________WHILE__________
-// let i = 0;
-// while (i < 2) {
-// 	let a = prompt("Обов'язкова стаття витрат", ''),
-// 		b = prompt("Скільки коштуватиме?", '');
-	
-// 	if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-// 		console.log('Done');
-// 		appData.expenses[a] = b;
-// 	} else {
-// 		console.log('Inpit error');
-// 		i--;
-// 	}
-
-// 	i++;
-// }
-
-
-
-// __________DO__________
-// let i = 0;
-// do {
-// 	let a = prompt("Обов'язкова стаття витрат", ''),
-// 		b = prompt("Скільки коштуватиме?", '');
-
-// 	if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-// 		console.log('Done');
-// 		appData.expenses[a] = b;
-// 	} else {
-// 		console.log('Inpit error');
-// 		i--;
-// 	}
-
-// 	i++;
-// }
-// while(i < 2);
+alert(appData.budget / 30);
